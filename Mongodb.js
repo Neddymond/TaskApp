964e23d7059df8bd8b4799cc0694fbafde9a4907
Chinedu codePanda Ikechi <Neddymond10@gmail.com>
2020-04-18 12:15:46 +0100

    Creates a new collection and inserts multiple documents into it.

--- Mongodb.js
+++ Mongodb.js
@@ -13,8 +13,46 @@ MongoClient.connect(connectionURL, { useUnifiedTopology: true}, (error, client) 
     /** Create a database using the databaseName we created earlier */
     const db = client.db(databaseName);
 
-    db.collection("Users").insertOne({
-        name: "Chinedu",
-        age: 23
+    // /** Create a "Users collection and insert one document into it" */
+    // db.collection("Users").insertOne({
+    //     name: "Chinedu",
+    //     age: 23
+    // }, (error, result) => {
+    //     if(error) return console.log("Couldn't insert the document into the collection. Kindly begin your deubgging process.");
+    //     console.log(result.ops);
+    // })
+
+    // /** Insert multiple documents into the Users collection */
+    // db.collection("Users").insertMany([
+    //     {
+    //         name: "Alonso",
+    //         occupation: "Animator"
+    //     },
+    //     {
+    //         name: "Emeka",
+    //         occupation: "Software Engineer"
+    //     }
+    // ], (error, result) => {
+    //     if(error) return console.log("couldn't insert document into the users collection. You can start your debugging process");
+    //     console.log(result.ops);
+    // })
+
+    /** Create a new collection and insert multiple documents */
+    db.collection("Todo").insertMany([
+        {
+            description: "Get a dope-ass six figure job",
+            completed: false
+        },
+        {
+            description: "Finish reading YDKJS",
+            completed: false
+        },
+        {
+            description: "Finish reading Unfuck yourself",
+            completed: true
+        }
+    ], (error, result) => {
+        if(error) return console.log("Couldn't insert the documents into the collection");
+        console.log(result.ops);
     })
 })
