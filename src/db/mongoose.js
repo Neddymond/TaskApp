0bbf23b920f76da5a509361e988a11f416163f9e
Chinedu codePanda Ikechi <Neddymond10@gmail.com>
2020-04-20 11:32:01 +0100

    Validates and sanitizes fields before saving

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 /** Connect to to database */
 mongoose.connect("mongodb://127.0.0.1:27017/TaskManager-Api", {
@@ -6,32 +7,53 @@ mongoose.connect("mongodb://127.0.0.1:27017/TaskManager-Api", {
     useCreateIndex: true
 });
 
-// /** Create a User model */
-// const User = mongoose.model("User", {
-//     name: { type: String},
-//     age: {type: Number}
-// });
-
-// /** Create an instance of the user model */
-// const me = new User({
-//     name: "chinedu",
-//     age: 24
-// });
-
-// /** Save the document to the database */
-// me.save().then(() => console.log(me)).catch((error) => {console.log("Error:", error)});
-
-/** Create a Task model */
-const Task = mongoose.model("Task", {
-    description: {type: String},
-    completed: {type: Boolean}
+/** Create a User model */
+const User = mongoose.model("User", {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Email is invalid");
+            }
+        }
+    },
+    age: {
+        type: Number,
+        default: 0, 
+        validate(value){
+            if(value < 0) throw new Error("Positive number required");
+        }
+    }
 });
 
-/** create an instance of the Task model */
-const todaysTask = new Task({
-    description: "Watch JavaScript tutorial",
-    completed: false
+/** Create an instance of the user model */
+const me = new User({
+    name: "chinedu",
+    email: "chineDu@gmail.coM"
 });
 
 /** Save the document to the database */
-todaysTask.save().then(() => console.log(todaysTask)).catch((error) => {console.log(error);})
+me.save().then(() => console.log(me)).catch((error) => {console.log("Error:", error)});
+
+// /** Create a Task model */
+// const Task = mongoose.model("Task", {
+//     description: {type: String},
+//     completed: {type: Boolean}
+// });
+
+// /** create an instance of the Task model */
+// const todaysTask = new Task({
+//     description: "Watch JavaScript tutorial",
+//     completed: false
+// });
+
+// /** Save the document to the database */
+// todaysTask.save().then(() => console.log(todaysTask)).catch((error) => {console.log(error);})
