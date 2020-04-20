ff95d27598f31fd04af5b86785b7b46b22a9c833
Chinedu codePanda Ikechi <Neddymond10@gmail.com>
2020-04-20 12:52:51 +0100

    Validates and sanitizes documents before saving them.

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,7 +3,7 @@ const validator = require("validator");
 
 /** Connect to to database */
 mongoose.connect("mongodb://127.0.0.1:27017/TaskManager-Api", {
-    useUnifiedTopology: true,
+    useNewUrlParser: true,
     useCreateIndex: true
 });
 
@@ -25,6 +25,17 @@ const User = mongoose.model("User", {
             }
         }
     },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 7,
+        validate(value){
+            if(value.toLowerCase().includes('password')){
+                throw new Error("Invalid password. Password cannot 'password'.")
+            }
+        }
+    },
     age: {
         type: Number,
         default: 0, 
@@ -35,25 +46,32 @@ const User = mongoose.model("User", {
 });
 
 /** Create an instance of the user model */
-const me = new User({
-    name: "chinedu",
-    email: "chineDu@gmail.coM"
+// const me = new User({
+//     name: "chinedu",
+//     email: "chineDu@gmail.coM",
+//     password: "Neddymond10"
+// });
+
+/** Save the document to the database */
+// me.save().then(() => console.log(me)).catch((error) => {console.log("Error:", error)});
+
+/** Create a Task model */
+const Task = mongoose.model("Task", {
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
+});
+
+/** create an instance of the Task model */
+const todaysTask = new Task({
+    description: "Write a medium article",
 });
 
 /** Save the document to the database */
-me.save().then(() => console.log(me)).catch((error) => {console.log("Error:", error)});
-
-// /** Create a Task model */
-// const Task = mongoose.model("Task", {
-//     description: {type: String},
-//     completed: {type: Boolean}
-// });
-
-// /** create an instance of the Task model */
-// const todaysTask = new Task({
-//     description: "Watch JavaScript tutorial",
-//     completed: false
-// });
-
-// /** Save the document to the database */
-// todaysTask.save().then(() => console.log(todaysTask)).catch((error) => {console.log(error);})
+todaysTask.save().then(() => console.log(todaysTask)).catch((error) => {console.log(error);})
