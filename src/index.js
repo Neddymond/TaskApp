5d6425a035f8d4b477ad37a3728dc17fdea1ab16
Chinedu codePanda Ikechi <Neddymond10@gmail.com>
2020-04-21 12:25:02 +0100

    Gets a single document by Id

--- src/index.js
+++ src/index.js
@@ -28,6 +28,17 @@ app.get("/users", (req, res) => {
     User.find({}).then((users) => res.send(users)).catch((e) => {res.status(500).send();});
 });
 
+/** Find a user */
+app.get("/users/:id", (req, res) => {
+    /** The Id to query for */
+    const _id = req.params.id;
+
+    User.findById(_id).then((user) => {
+        /** Return 404 error if user is not found */
+        user ? res.send(user) : res.status(404).send();
+    }).catch((e) => res.status(500).send());
+})
+
 /** Task creation endpoint using post request*/
 app.post("/tasks", (req, res) => {
     const task = new Task(req.body);
