7540d3266036fbc36dbdd7f7827cdd1ef73d45e6
Chinedu codePanda Ikechi <Neddymond10@gmail.com>
2020-04-22 14:02:56 +0100

    Refactors the code using async/await

--- src/index.js
+++ src/index.js
@@ -13,53 +13,78 @@ app.use(express.json());
 /** Port */
 const port = process.env.PORT  || 3000;
 
-/** User creation endpoint using post request*/
-app.post("/users", (req, res) => {
+/**Endpoint for creating a user using a route pointer*/
+app.post("/users", async (req, res) => {
     const user = new User(req.body);
-
     /** Save user to the database */
-    user.save().then(() => res.status(201).send(user)).catch((e) => {
+    try{
+        await user.save();
+        res.status(201).send(user);
+    }catch (e) {
         res.status(400).send(e);
-    });
+    }
 });
 
 /** Endpoint for fetching multiple users*/
-app.get("/users", (req, res) => {
-    User.find({}).then((users) => res.send(users)).catch((e) => {res.status(500).send();});
+app.get("/users", async (req, res) => {
+    try{
+        const users = await User.find({});
+        res.send(users);
+    }catch (e) {
+        res.status(500).send(e);
+    }
 });
 
 /** Endpoint for fetching a user using a route parameter */
-app.get("/users/:id", (req, res) => {
+app.get("/users/:id", async (req, res) => {
     /** The Id to query for */
     const _id = req.params.id;
 
-    User.findById(_id).then((user) => {
+    try{
+        const user = await User.findById(_id);
+
         /** Return 404 error if user is not found */
         user ? res.send(user) : res.status(404).send();
-    }).catch((e) => res.status(500).send());
+    }catch (e) {
+        res.status(500).send(e);
+    }
 });
 
 /** Task creation endpoint using post request*/
-app.post("/tasks", (req, res) => {
+app.post("/tasks", async (req, res) => {
     const task = new Task(req.body);
 
-    /** save task to the database */
-    task.save().then(() => res.status(201).send(task)).catch((e) => { res.status(400).send(e);});
+    try{
+        /** save task to the database */
+        await task.save();
+        res.status(201).send(task);
+    }catch (e) {
+        res.status(400).send(e);
+    }
 });
 
 /** Endpont for fetching multiple tasks */
-app.get("/tasks", (req, res) => {
-    Task.find({}).then((tasks) => res.send(tasks)).catch((e) => res.status(500).send());
+app.get("/tasks", async (req, res) => {
+    try{
+        /** Get all tasks */
+        const tasks = await Task.find({});
+        res.send(tasks);
+    }catch (e) {
+        res.status(500).send(e);
+    }
 });
 
 /** Endpoint for fetching a task using a route parameter*/
-app.get("/tasks/:id", (req, res) => {
+app.get("/tasks/:id", async(req, res) => {
     const _id = req.params.id;
 
-    Task.findById(_id).then((task) => {
-        /** Returns task if found, else, return 404 error */
-        task ? res.send(task) : res.status(404).send();
-    }).catch((e) => res.status(500).send());
+    try{
+        /** Get a task by id */
+        const task = await Task.findById(_id);
+        task ? res.send(task) : res.status(400).send();
+    }catch (e) {
+        res.status(500).send(e);
+    }
 });
 
 app.listen(port, () => {
